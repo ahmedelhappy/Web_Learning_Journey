ac745e871d001ab34eeffded29f16f7f9415dac5
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-12 07:57:48 +0300

    Getting started with BOM

--- JS/main.js
+++ JS/main.js
@@ -1454,6 +1454,30 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 
 
 
-//================================================
+//======================BOM==========================
+/*
+  BOM [Browser Object Model]
+  - alert(Message) => Need No Response Only Ok Available
+  - confirm(Message) => Need Response And Return A Boolean
+  - prompt(Message, Default Message) => Collect Data
+*/
+
+// alert("Test");
+// console.log("Test");
+
+// let confirmMsg = confirm("Are You Sure?");
+
+// console.log(confirmMsg);
+
+// if (confirmMsg === true) {
+//   console.log("Item Deleted");
+// } else {
+//   console.log("Item Not Deleted");
+// }
+
+// let promptMsg = prompt("Enter Your birth year", "Default");
+
+// console.log(promptMsg);
+
 //================================================
 
