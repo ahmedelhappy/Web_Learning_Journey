52ccf8505d62cb43fe20814762943640a0f29b03
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-15 15:31:45 +0300

    Destructuring, objects & arrays

--- JS/main.js
+++ JS/main.js
@@ -1862,30 +1862,57 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 // console.log(`My HTML Skill Progress Is ${skillOne}`);
 // console.log(`My CSS Skill Progress Is ${skillTwo}`);
 
+// /*
+//   Destructuring
+//   - Destructuring Function Parameters
+// */
+
+// const user = {
+//   theName: "Osama",
+//   theAge: 39,
+//   skills: {
+//     html: 70,
+//     css: 80,
+//   },
+// };
+
+// showDetails(user);
+
+// // function showDetails(obj) {
+// //   console.log(`Your Name Is ${obj.theName}`);
+// //   console.log(`Your Age Is ${obj.theAge}`);
+// //   console.log(`Your CSS Skill Progress Is ${obj.skills.css}`);
+// // }
+
+// function showDetails({ theName: n, theAge: a, skills: { css: c } } = user) {
+//   console.log(`Your Name Is ${n}`);
+//   console.log(`Your Age Is ${a}`);
+//   console.log(`Your CSS Skill Progress Is ${c}`);
+// }
+
 /*
   Destructuring
-  - Destructuring Function Parameters
+  - Destructuring Mixed Content
 */
 
 const user = {
   theName: "Osama",
   theAge: 39,
-  skills: {
-    html: 70,
-    css: 80,
+  skills: ["HTML", "CSS", "JavaScript"],
+  addresses: {
+    egypt: "Cairo",
+    ksa: "Riyadh",
   },
 };
 
-showDetails(user);
+const {
+  theName: n,
+  theAge: a,
+  skills: [, , three],
+  addresses: { egypt: e },
+} = user;
 
-// function showDetails(obj) {
-//   console.log(`Your Name Is ${obj.theName}`);
-//   console.log(`Your Age Is ${obj.theAge}`);
-//   console.log(`Your CSS Skill Progress Is ${obj.skills.css}`);
-// }
-
-function showDetails({ theName: n, theAge: a, skills: { css: c } } = user) {
-  console.log(`Your Name Is ${n}`);
-  console.log(`Your Age Is ${a}`);
-  console.log(`Your CSS Skill Progress Is ${c}`);
-}
+console.log(`Your Name Is: ${n}`);
+console.log(`Your Age Is: ${a}`);
+console.log(`Your Last Skill Is: ${three}`);
+console.log(`Your Live In: ${e}`);
