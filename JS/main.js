855f7f7934aa2f4c2a60ab29905cb2c1a84a33c6
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-18 19:09:29 +0300

    Array every Method, Mutating VS Non-mutating. added a comparison btw Mutating VS Non-mutating in the Notes.txt

--- JS/main.js
+++ JS/main.js
@@ -2238,58 +2238,101 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 
 // console.log(myArray);
 //===============================================
+// /*
+//   Array Methods
+//   - Array.some(CallbackFunc(Element, Index, Array), This Argument)
+//   --- CallbackFunc => Function To Run On Every Element On The Given Array
+//   ------ Element => The Current Element To Process
+//   ------ Index => Index Of Current Element
+//   ------ Array => The Current Array Working With
+//   --- This Argument => Value To Use As This When Executing CallbackFunc
+//   --
+//   Using
+//   - Check if Element Exists In Array
+//   - Check If Number In Range
+// */
+
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let myNumber = 10;
+
+// // let check = nums.some(function (e) {
+// //   console.log("Test");
+// //   return e > 5;
+// // });
+
+// let check = nums.some(function (e) {
+//   return e > this;
+// }, myNumber);
+
+// // let check = nums.some((e) => e > 5);
+
+// console.log(check);
+
+// function checkValues(arr, val) {
+//   return arr.some(function (e) {
+//     return e === val;
+//   });
+// }
+
+// console.log(checkValues(nums, 20));
+// console.log(checkValues(nums, 5));
+
+// let range = {
+//   min: 10,
+//   max: 20,
+// };
+
+// let checkNumberInRange = nums.some(function (e) {
+//   // console.log(this.min);
+//   // console.log(this.max);
+//   return e >= this.min && e <= this.max;
+// }, range);
+
+// console.log(checkNumberInRange);
+//===================================
+
 /*
   Array Methods
-  - Array.some(CallbackFunc(Element, Index, Array), This Argument)
+  - Array.every(CallbackFunc(Element, Index, Array), This Argument)
   --- CallbackFunc => Function To Run On Every Element On The Given Array
   ------ Element => The Current Element To Process
   ------ Index => Index Of Current Element
   ------ Array => The Current Array Working With
   --- This Argument => Value To Use As This When Executing CallbackFunc
   --
-  Using
-  - Check if Element Exists In Array
-  - Check If Number In Range
 */
 
-let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const locations = {
+  20: "Place 1",
+  30: "Place 2",
+  50: "Place 3",
+  40: "Place 4",
+};
 
-let myNumber = 10;
+let mainLocation = 15;
 
-// let check = nums.some(function (e) {
-//   console.log("Test");
-//   return e > 5;
-// });
+let locationsArray = Object.keys(locations);
 
-let check = nums.some(function (e) {
+console.log(locationsArray);
+
+let locationArrayNumbers = locationsArray.map((n) => +n);
+
+console.log(locationArrayNumbers);
+
+let check = locationArrayNumbers.every(function (e) {
   return e > this;
-}, myNumber);
-
-// let check = nums.some((e) => e > 5);
+}, mainLocation);
 
 console.log(check);
 
-function checkValues(arr, val) {
-  return arr.some(function (e) {
-    return e === val;
-  });
-}
 
-console.log(checkValues(nums, 20));
-console.log(checkValues(nums, 5));
 
-let range = {
-  min: 10,
-  max: 20,
-};
 
-let checkNumberInRange = nums.some(function (e) {
-  // console.log(this.min);
-  // console.log(this.max);
-  return e >= this.min && e <= this.max;
-}, range);
 
-console.log(checkNumberInRange);
+
+
+
 
 
 
