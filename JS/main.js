3bca42a23b7de120d9e4043f0ae224bdb75cc16e
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-18 19:33:57 +0300

    Regular Expression Intro

--- JS/main.js
+++ JS/main.js
@@ -2326,63 +2326,82 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 
 // console.log(check);
 //=======================================
+// /*
+//   Spread Operator => ...Iterable
+//   "Allow Iterable To Expand In Place"
+// */
+
+// // Spread With String => Expand String
+
+// console.log("Osama");
+// console.log(..."Osama");
+// console.log([..."Osama"]);
+
+// // Concatenate Arrays
+
+// let myArray1 = [1, 2, 3];
+// let myArray2 = [4, 5, 6];
+
+// let allArrays = [...myArray1, ...myArray2];
+// console.log(allArrays);
+
+// // Copy Array
+
+// let copiedArray = [...myArray1];
+// console.log(copiedArray);
+
+// // Push Inside Array
+
+// let allFriends = ["Osama", "Ahmed", "Sayed"];
+// let thisYearFriends = ["Sameh", "Mahmoud"];
+
+// allFriends.push(...thisYearFriends);
+
+// console.log(allFriends);
+
+// // Use With Math Object
+
+// let myNums = [10, 20, -100, 100, 1000, 500];
+// console.log(Math.max(...myNums));
+
+// // Spread With Objects => Merge Objects
+
+// let objOne = {
+//   a: 1,
+//   b: 2,
+// };
+// let objTwo = {
+//   c: 3,
+//   d: 4,
+// };
+
+// console.log({ ...objOne, ...objTwo, e: 5 });
+
+//==============================================
+
 /*
-  Spread Operator => ...Iterable
-  "Allow Iterable To Expand In Place"
+  Regular Expression Intro
+  - Email
+  - IP
+  - Phone
+  - URL
 */
 
-// Spread With String => Expand String
+let str1 = '10 20 100 1000 5000';
+let str2 = 'Os1 Os12 Os123 Os123Os Os12312Os123';
 
-console.log("Osama");
-console.log(..."Osama");
-console.log([..."Osama"]);
+let invalidEmail = 'Osama@@@gmail....com';
+let validEmail = 'o@nn.sa';
 
-// Concatenate Arrays
+let ip = '192.168.2.1'; // IPv4
 
-let myArray1 = [1, 2, 3];
-let myArray2 = [4, 5, 6];
-
-let allArrays = [...myArray1, ...myArray2];
-console.log(allArrays);
-
-// Copy Array
-
-let copiedArray = [...myArray1];
-console.log(copiedArray);
-
-// Push Inside Array
-
-let allFriends = ["Osama", "Ahmed", "Sayed"];
-let thisYearFriends = ["Sameh", "Mahmoud"];
-
-allFriends.push(...thisYearFriends);
-
-console.log(allFriends);
-
-// Use With Math Object
-
-let myNums = [10, 20, -100, 100, 1000, 500];
-console.log(Math.max(...myNums));
-
-// Spread With Objects => Merge Objects
-
-let objOne = {
-  a: 1,
-  b: 2,
-};
-let objTwo = {
-  c: 3,
-  d: 4,
-};
-
-console.log({ ...objOne, ...objTwo, e: 5 });
-
-
-
-
-
-
-
+let url = 'elzero.org';
+url = 'elzero.org/';
+url = 'http://elzero.org/';
+url = 'http://www.elzero.org/';
+url = 'https://.elzero.org/';
+url = 'https://www.elzero.org/';
+url = 'https://www.elzero.org/?facebookid=asdasdasd';
 
 
 
