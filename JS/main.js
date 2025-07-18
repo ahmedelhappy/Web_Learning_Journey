2555fa55fcc458908aede7daf1d70aac7937ec41
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-18 18:43:42 +0300

    Array some method Practice.

--- JS/main.js
+++ JS/main.js
@@ -2207,41 +2207,89 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 // console.log(af("Osama", "Ahmed", "sayed", 1, 2, 3));
 
 //=====================================
+// /*
+//   Array Methods
+//   - Array.copyWithin(Target, Start => Optional, End => Optional)
+//   "Copy Part Of An Array To Another Location in The Same Array"
+//   -- Any Negative Value Will Count From The End
+//   -- Target
+//   ---- Index To Copy Part To
+//   ---- If At Or Greater Than Array Length Nothing Will Be Copied
+//   -- Start
+//   ---- Index To Start Copying From
+//   ---- If Ommited = Start From Index 0
+//   -- End
+//   ---- Index To End Copying From
+//   ---- Not Including End
+//   ---- If Ommited = Reach The End
+// */
+
+// let myArray = [10, 20, 30, 40, 50, "A", "B"];
+
+// // myArray.copyWithin(3); // [10, 20, 30, 10, 20, 30, 40]
+
+// // myArray.copyWithin(4, 6); // [10, 20, 30, 40, "B", "A", "B"]
+
+// // myArray.copyWithin(4, -1); // [10, 20, 30, 40, "B", "A", "B"]
+
+// // myArray.copyWithin(1, -2); // [10, "A", "B", 40, 50, "A", "B"]
+
+// myArray.copyWithin(1, -2, -1); // [10, "A", 30, 40, 50, "A", "B"]
+
+// console.log(myArray);
+//===============================================
 /*
   Array Methods
-  - Array.copyWithin(Target, Start => Optional, End => Optional)
-  "Copy Part Of An Array To Another Location in The Same Array"
-  -- Any Negative Value Will Count From The End
-  -- Target
-  ---- Index To Copy Part To
-  ---- If At Or Greater Than Array Length Nothing Will Be Copied
-  -- Start
-  ---- Index To Start Copying From
-  ---- If Ommited = Start From Index 0
-  -- End
-  ---- Index To End Copying From
-  ---- Not Including End
-  ---- If Ommited = Reach The End
+  - Array.some(CallbackFunc(Element, Index, Array), This Argument)
+  --- CallbackFunc => Function To Run On Every Element On The Given Array
+  ------ Element => The Current Element To Process
+  ------ Index => Index Of Current Element
+  ------ Array => The Current Array Working With
+  --- This Argument => Value To Use As This When Executing CallbackFunc
+  --
+  Using
+  - Check if Element Exists In Array
+  - Check If Number In Range
 */
 
-let myArray = [10, 20, 30, 40, 50, "A", "B"];
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// myArray.copyWithin(3); // [10, 20, 30, 10, 20, 30, 40]
+let myNumber = 10;
 
-// myArray.copyWithin(4, 6); // [10, 20, 30, 40, "B", "A", "B"]
+// let check = nums.some(function (e) {
+//   console.log("Test");
+//   return e > 5;
+// });
 
-// myArray.copyWithin(4, -1); // [10, 20, 30, 40, "B", "A", "B"]
+let check = nums.some(function (e) {
+  return e > this;
+}, myNumber);
 
-// myArray.copyWithin(1, -2); // [10, "A", "B", 40, 50, "A", "B"]
+// let check = nums.some((e) => e > 5);
 
-myArray.copyWithin(1, -2, -1); // [10, "A", 30, 40, 50, "A", "B"]
+console.log(check);
 
-console.log(myArray);
+function checkValues(arr, val) {
+  return arr.some(function (e) {
+    return e === val;
+  });
+}
 
+console.log(checkValues(nums, 20));
+console.log(checkValues(nums, 5));
 
+let range = {
+  min: 10,
+  max: 20,
+};
 
+let checkNumberInRange = nums.some(function (e) {
+  // console.log(this.min);
+  // console.log(this.max);
+  return e >= this.min && e <= this.max;
+}, range);
 
-
+console.log(checkNumberInRange);
 
 
 
