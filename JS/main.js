1a2f6d940a012ccc9cae0b7d64f31a8b59c85295
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-18 16:26:46 +0300

    WeakSet

--- JS/main.js
+++ JS/main.js
@@ -1948,36 +1948,89 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
   - has
 */
 
-let myData = [1, 1, 1, 2, 3, "A"];
-// let myUniqueData = new Set([1, 1, 1, 2, 3]);
-// let myUniqueData = new Set(myData);
-// let myUniqueData = new Set().add(1).add(1).add(1).add(2).add(3);
-let myUniqueData = new Set();
+// let myData = [1, 1, 1, 2, 3, "A"];
+// // let myUniqueData = new Set([1, 1, 1, 2, 3]);
+// // let myUniqueData = new Set(myData);
+// // let myUniqueData = new Set().add(1).add(1).add(1).add(2).add(3);
+// let myUniqueData = new Set();
 
-myUniqueData.add(1).add(1).add(1);
-myUniqueData.add(2).add(3).add("A");
+// myUniqueData.add(1).add(1).add(1);
+// myUniqueData.add(2).add(3).add("A");
 
-console.log(`Is Set Has => A ${myUniqueData.has("a".toUpperCase())}`);
+// console.log(`Is Set Has => A ${myUniqueData.has("a".toUpperCase())}`);
 
-console.log(myData);
-console.log(myUniqueData);
+// console.log(myData);
+// console.log(myUniqueData);
 
-console.log(myUniqueData.size);
+// console.log(myUniqueData.size);
 
-console.log(myData[0]);
-console.log(myUniqueData[0]);
+// console.log(myData[0]);
+// console.log(myUniqueData[0]);
 
-// myUniqueData.delete(2);
-console.log(myUniqueData.delete(2));
+// // myUniqueData.delete(2);
+// console.log(myUniqueData.delete(2));
 
-console.log(myUniqueData);
-console.log(myUniqueData.size);
+// console.log(myUniqueData);
+// console.log(myUniqueData.size);
 
-myUniqueData.clear();
+// myUniqueData.clear();
 
-console.log(myUniqueData);
-console.log(myUniqueData.size);
+// console.log(myUniqueData);
+// console.log(myUniqueData.size);
+//============================================
+// /*
+//   - Set vs WeakSet
+//   "
+//     The WeakSet is weak,
+//     meaning references to objects in a WeakSet are held weakly.
+//     If no other references to an object stored in the WeakSet exist,
+//     those objects can be garbage collected.
+//   "
+//   --
+//   Set     => Can Store Any Data Values
+//   WeakSet => Collection Of Objects Only
+//   --
+//   Set     => Have Size Property
+//   WeakSet => Does Not Have Size Property
+//   --
+//   Set     => Have Keys, Values, Entries
+//   WeakSet => Does Not Have clear, Keys, Values And Entries
+//   --
+//   Set     => Can Use forEach
+//   WeakSet => Cannot Use forEach
 
+//   Usage: Store objects and removes them once they become inaccessible
+// */
+
+// // Type Of Data
+
+// let mySet = new Set([1, 1, 1, 2, 3, "A", "A"]);
+
+// console.log(mySet);
+
+// // Size
+// console.log(`Size Of Elements Inside Set Is: ${mySet.size}`);
+
+// // Values + Keys [Alias For Values]
+// let iterator = mySet.keys();
+
+// console.log(iterator.next().value);
+// console.log(iterator.next().value);
+// console.log(iterator.next().value);
+// console.log(iterator.next().value);
+// console.log(iterator.next());
+
+// // forEach
+
+// mySet.forEach((el) => console.log(el));
+
+// console.log("#".repeat(20));
+
+// // Type Of Data
+
+// let myws = new WeakSet([{ A: 1, B: 2 }]);
+
+// console.log(myws);
 
 
 
