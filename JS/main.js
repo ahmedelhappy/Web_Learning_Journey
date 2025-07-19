e11b166bd09cee02c43ab82f7b5f71475d49711a
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-19 18:17:47 +0300

    Regular Expressions Practice

--- JS/main.js
+++ JS/main.js
@@ -2434,47 +2434,89 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 // console.log(myString.match(regex2));
 //===========================================
 
-/*
-  Regular Expression
+// /*
+//   Regular Expression
 
-  Ranges
+//   Ranges
 
-  - Part 1
-  (X|Y) => X Or Y
-  [0-9] => 0 To 9
-  [^0-9] => Any Character Not 0 To 9
-  Practice
+//   - Part 1
+//   (X|Y) => X Or Y
+//   [0-9] => 0 To 9
+//   [^0-9] => Any Character Not 0 To 9
+//   Practice
 
-  - Part 2
-  [a-z]
-  [^a-z]
-  [A-Z]
-  [^A-Z]
-  [abc]
-  [^abc]
+//   - Part 2
+//   [a-z]
+//   [^a-z]
+//   [A-Z]
+//   [^A-Z]
+//   [abc]
+//   [^abc]
 
-*/
+// */
 
-let tld = "Com Net Org Info Code Io";
-let tldRe = /(info|org|io)/ig;
-console.log(tld.match(tldRe));
+// let tld = "Com Net Org Info Code Io";
+// let tldRe = /(info|org|io)/ig;
+// console.log(tld.match(tldRe));
 
-let nums = "12345678910";
-let numsRe = /[0-2]/g;
-console.log(nums.match(numsRe));
+// let nums = "12345678910";
+// let numsRe = /[0-2]/g;
+// console.log(nums.match(numsRe));
 
-let notNums = "12345678910";
-let notNsRe = /[^0-2]/g;
-console.log(notNums.match(notNsRe));
+// let notNums = "12345678910";
+// let notNsRe = /[^0-2]/g;
+// console.log(notNums.match(notNsRe));
 
-let specialNums = "1!2@3#4$5%678910";
-let specialNumsRe = /[^0-9]/g;
-console.log(specialNums.match(specialNumsRe));
+// let specialNums = "1!2@3#4$5%678910";
+// let specialNumsRe = /[^0-9]/g;
+// console.log(specialNums.match(specialNumsRe));
 
-let practice = "Os1 Os1Os Os2 Os8 Os8Os";
-let practiceRe = /Os[5-9]Os/g;
-console.log(practice.match(practiceRe));
+// let practice = "Os1 Os1Os Os2 Os8 Os8Os";
+// let practiceRe = /Os[5-9]Os/g;
+// console.log(practice.match(practiceRe));
 
+
+//=================
+// /*
+//   Regular Expression
+
+//   Ranges
+
+//   - Part 1
+//   (X|Y) => X Or Y
+//   [0-9] => 0 To 9
+//   [^0-9] => Any Character Not 0 To 9
+//   Practice
+
+//   - Part 2
+//   [a-z]
+//   [^a-z]
+//   [A-Z]
+//   [^A-Z]
+//   [abc]
+//   [^abc]
+
+// */
+
+// let myString = "AaBbcdefG123!234%^&*";
+// let atozSmall = /[a-z]/g;
+// let NotAtozSmall = /[^a-z]/g;
+// let atozCapital = /[A-Z]/g;
+// let NotAtozCapital = /[^A-Z]/g;
+// let aAndcAnde = /[ace]/g;
+// let NotaAndcAnde = /[^ace]/g;
+// let lettersCapsAndSmall = /[a-zA-Z]/g;
+// let numsAndSpecials = /[^a-zA-Z]/g;
+// let specials = /[^a-zA-Z0-9]/g;
+// console.log(myString.match(atozSmall));
+// console.log(myString.match(NotAtozSmall));
+// console.log(myString.match(atozCapital));
+// console.log(myString.match(NotAtozCapital));
+// console.log(myString.match(aAndcAnde));
+// console.log(myString.match(NotaAndcAnde));
+// console.log(myString.match(lettersCapsAndSmall));
+// console.log(myString.match(numsAndSpecials));
+// console.log(myString.match(specials));
 
 
 
