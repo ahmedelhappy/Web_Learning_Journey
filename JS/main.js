1c5252b3ecb156d0cb2f2d91b6c4327e3d67cdf3
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-20 00:47:22 +0300

    Object Meta Data And Descriptor

--- JS/main.js
+++ JS/main.js
@@ -2984,9 +2984,76 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 
 //=================
 //=================
+// /*
+//   Object Meta Data And Descriptor
+//   - writable
+//   - enumerable
+//   - configurable [Cannot Delete Or Reconfigure]
+// */
+////well this adds a property to an object not a class right?
+
+// const myObject = {
+//   a: 1,
+//   b: 2,
+// };
+
+// Object.defineProperty(myObject, "c", {
+//   writable: false,
+//   enumerable: true,
+//   configurable: false,
+//   value: 3,
+// });
+
+// // Object.defineProperty(myObject, "c", {
+// //   writable: false,
+// //   enumerable: true,
+// //   configurable: true, <= Cannot redefine property
+// //   value: 3,
+// // });
+
+// myObject.c = 100;
+
+// console.log(delete myObject.c);
+
+// for (let prop in myObject) {
+//   console.log(prop, myObject[prop]);
+// }
+
+// console.log(myObject);
 
 
+//=================
+//=================
+/*
+  Object Meta Data And Descriptor
+  - Define Multiple Properties
+  - Check Descriptors
+*/
 
+const myObject = {
+  a: 1,
+  b: 2,
+};
+
+Object.defineProperties(myObject, {
+  c: {
+    configurable: true,
+    value: 3,
+  },
+  d: {
+    configurable: true,
+    value: 4,
+  },
+  e: {
+    configurable: true,
+    value: 5,
+  },
+});
+
+console.log(myObject);
+
+console.log(Object.getOwnPropertyDescriptor(myObject, "d"));
+console.log(Object.getOwnPropertyDescriptors(myObject));
 //=================
 //=================
 
@@ -3024,6 +3091,6 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
         return `Hello ${this.u} Your Salary Is ${this.s}`;
       };
 
-  - [ ]
+  - [ ]  Prototypes are a bit vague...
   - [ ]
 */
