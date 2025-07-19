13e51c55e1de99d4205e58dabbb23e42b1ac0fef
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-19 18:43:06 +0300

    Character Classes, Test method

--- JS/main.js
+++ JS/main.js
@@ -2519,27 +2519,46 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 // console.log(myString.match(specials));
 
 //==========================================
+// /*
+//   Regular Expression
+//   Character Classes
+//   . => matches any character, except newline or other line terminators.
+//   \w => matches word characters. [a-z, A-Z, 0-9 And Underscore]
+//   \W => matches Non word characters
+//   \d => matches digits from 0 to 9.
+//   \D => matches non-digit characters.
+//   \s => matches whitespace character.
+//   \S => matches non whitespace character.
+// */
+
+// let email = 'O@@@g...com O@g.com O@g.net A@Y.com O-g.com o@s.org 1@1.com';
+// let dot = /./g;
+// let word = /\w/g;
+// let valid = /\w@\w.(com|net)/g;
+// console.log(email.match(dot));
+// console.log(email.match(word));
+// console.log(email.match(valid));
+
+
+//==========================================
 /*
   Regular Expression
   Character Classes
-  . => matches any character, except newline or other line terminators.
-  \w => matches word characters. [a-z, A-Z, 0-9 And Underscore]
-  \W => matches Non word characters
-  \d => matches digits from 0 to 9.
-  \D => matches non-digit characters.
-  \s => matches whitespace character.
-  \S => matches non whitespace character.
+  \b => matches at the beginning or end of a word.
+  \B => matches NOT at the beginning/end of a word.
+
+  Test Method
+  pattern.test(input)
 */
 
-let email = 'O@@@g...com O@g.com O@g.net A@Y.com O-g.com o@s.org 1@1.com';
-let dot = /./g;
-let word = /\w/g;
-let valid = /\w@\w.(com|net)/g;
-console.log(email.match(dot));
-console.log(email.match(word));
-console.log(email.match(valid));
+let names = "Sayed 1Spam 2Spam 3Spam Spam4 Spam5 Osama Ahmed Aspamo";
+let re = /(\bspam|spam\b)/ig;
+console.log(names.match(re));
 
-
+console.log(re.test(names));
+console.log(/(\bspam|spam\b)/ig.test("Osama"));
+console.log(/(\bspam|spam\b)/ig.test("1Spam"));
+console.log(/(\bspam|spam\b)/ig.test("Spam1"));
 
 
 
