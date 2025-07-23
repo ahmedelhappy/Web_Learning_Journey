4de00780f6167b6c69eda99c0afa9e4dead676b7
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-23 08:56:12 +0300

    Set Time.

--- JS/main.js
+++ JS/main.js
@@ -3093,35 +3093,71 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 // console.log(`Years ${years}`);
 //=================
 //=================
+// /*
+//   Date And Time
+//   - getTime() => Number Of Milliseconds
+//   - getDate() => Day Of The Month
+//   - getFullYear()
+//   - getMonth() => Zero Based
+//   - getDay() => Day Of The Week
+//   - getHours()
+//   - getMinutes()
+//   - getSeconds()
+// */
+
+// let dateNow = new Date();
+// let birthday = new Date("1, jan, 2004");
+// let dateDiff = dateNow - birthday;
+
+// console.log(dateDiff);
+// console.log(dateDiff / 1000 / 60 / 60 / 24 / 365);
+
+// console.log(dateNow);
+// console.log(dateNow.getTime());
+// console.log(dateNow.getDate());
+// console.log(dateNow.getFullYear());
+// console.log(dateNow.getMonth());
+// console.log(dateNow.getDay());
+// console.log(dateNow.getHours());
+// console.log(dateNow.getMinutes());
+// console.log(dateNow.getSeconds());
+
+//=================
+//=================
 /*
   Date And Time
-  - getTime() => Number Of Milliseconds
-  - getDate() => Day Of The Month
-  - getFullYear()
-  - getMonth() => Zero Based
-  - getDay() => Day Of The Week
-  - getHours()
-  - getMinutes()
-  - getSeconds()
+  - setTime(Milliseconds)
+  - setDate() => Day Of The Month [Negative And Positive]
+  - setFullYear(year, month => Optional [0-11], day => Optional [1-31])
+  - setMonth(Month [0-11], Day => Optional [1-31]) [Negative And Positive]
+  - setHours(Hours [0-23], Minutes => Optional [0-59], Seconds => Optional [0-59], MS => Optional [0-999])
+  - setMinutes(Minutes [0-59], Seconds => Optional [0-59], MS => Optional [0-999])
+  - setSeconds(Seconds => [0-59], MS => Optional [0-999])
 */
 
 let dateNow = new Date();
-let birthday = new Date("1, jan, 2004");
-let dateDiff = dateNow - birthday;
-
-console.log(dateDiff);
-console.log(dateDiff / 1000 / 60 / 60 / 24 / 365);
-
 console.log(dateNow);
-console.log(dateNow.getTime());
-console.log(dateNow.getDate());
-console.log(dateNow.getFullYear());
-console.log(dateNow.getMonth());
-console.log(dateNow.getDay());
-console.log(dateNow.getHours());
-console.log(dateNow.getMinutes());
-console.log(dateNow.getSeconds());
 
+console.log("#".repeat(66));
+
+// dateNow.setTime(0);
+// console.log(dateNow);
+
+// console.log("#".repeat(66));
+
+// dateNow.setTime(10000);
+// console.log(dateNow);
+
+// console.log("#".repeat(66));
+
+// dateNow.setDate(35);
+// console.log(dateNow);
+
+// dateNow.setFullYear(2020, 13);
+// console.log(dateNow);
+
+dateNow.setMonth(15);
+console.log(dateNow);
 //=================
 //=================
 
