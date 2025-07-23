6cb37e470cec70f76b93e384fe54681f42bb0146
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-23 11:39:58 +0300

    Delegate Generator

--- JS/main.js
+++ JS/main.js
@@ -3241,39 +3241,81 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 
 //=================
 //=================
+// /*
+//   Generators
+//   - Generator Function Run Its Code When Required.
+//   - Generator Function Return Special Object [Generator Object]
+//   - Generators Are Iterable
+// */
+
+// function* generateNumbers() {
+  //   yield 1;
+  //   console.log("Hello After Yield 1");
+  //   yield 2;
+  //   yield 3;
+  //   yield 4;
+  // }
+  
+  // let generator = generateNumbers();
+
+  // console.log(typeof generator);
+  // console.log(generator);
+  
+  // console.log(generator.next());
+  // console.log(generator.next());
+  // console.log(generator.next());
+  // console.log(generator.next());
+  // console.log(generator.next());
+  
+  // for (let value of generateNumbers()) {
+    //   console.log(value);
+    // }
+    
+    // for (let value of generator) {
+      //   console.log(value);
+      // }
+//=================
+//=================
 /*
   Generators
-  - Generator Function Run Its Code When Required.
-  - Generator Function Return Special Object [Generator Object]
-  - Generators Are Iterable
+  - Delegate Generator
 */
 
-function* generateNumbers() {
+function* generateNums() {
   yield 1;
-  console.log("Hello After Yield 1");
   yield 2;
   yield 3;
-  yield 4;
 }
 
-let generator = generateNumbers();
-
-console.log(typeof generator);
-console.log(generator);
-
-console.log(generator.next());
-console.log(generator.next());
-console.log(generator.next());
-console.log(generator.next());
-console.log(generator.next());
-
-for (let value of generateNumbers()) {
-  console.log(value);
+function* generateLetters() {
+  yield "A";
+  yield "B";
+  yield "C";
 }
 
-for (let value of generator) {
-  console.log(value);
+function* generateAll() {
+  yield* generateNums();
+  yield* generateLetters();
+  yield* [4, 5, 6];
 }
+
+let generator = generateAll();
+
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.return("Z"));
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+
+//=================
+//=================
+
+
 
 
 
