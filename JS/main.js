fdb62f9cfcd3892ea23530bdcb9f35b06b84ca70
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-23 09:17:45 +0300

    Track Operations Time

--- JS/main.js
+++ JS/main.js
@@ -3124,40 +3124,121 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 
 //=================
 //=================
-/*
-  Date And Time
-  - setTime(Milliseconds)
-  - setDate() => Day Of The Month [Negative And Positive]
-  - setFullYear(year, month => Optional [0-11], day => Optional [1-31])
-  - setMonth(Month [0-11], Day => Optional [1-31]) [Negative And Positive]
-  - setHours(Hours [0-23], Minutes => Optional [0-59], Seconds => Optional [0-59], MS => Optional [0-999])
-  - setMinutes(Minutes [0-59], Seconds => Optional [0-59], MS => Optional [0-999])
-  - setSeconds(Seconds => [0-59], MS => Optional [0-999])
-*/
+// /*
+//   Date And Time
+//   - setTime(Milliseconds)
+//   - setDate() => Day Of The Month [Negative And Positive]
+//   - setFullYear(year, month => Optional [0-11], day => Optional [1-31])
+//   - setMonth(Month [0-11], Day => Optional [1-31]) [Negative And Positive]
+//   - setHours(Hours [0-23], Minutes => Optional [0-59], Seconds => Optional [0-59], MS => Optional [0-999])
+//   - setMinutes(Minutes [0-59], Seconds => Optional [0-59], MS => Optional [0-999])
+//   - setSeconds(Seconds => [0-59], MS => Optional [0-999])
+// */
 
-let dateNow = new Date();
-console.log(dateNow);
-
-console.log("#".repeat(66));
-
-// dateNow.setTime(0);
+// let dateNow = new Date();
 // console.log(dateNow);
 
 // console.log("#".repeat(66));
 
-// dateNow.setTime(10000);
+// // dateNow.setTime(0);
+// // console.log(dateNow);
+
+// // console.log("#".repeat(66));
+
+// // dateNow.setTime(10000);
+// // console.log(dateNow);
+
+// // console.log("#".repeat(66));
+
+// // dateNow.setDate(35);
+// // console.log(dateNow);
+
+// // dateNow.setFullYear(2020, 13);
+// // console.log(dateNow);
+
+// dateNow.setMonth(15);
 // console.log(dateNow);
+//=================
+//=================
+// /*
+//   Date And Time
 
-// console.log("#".repeat(66));
+//   new Date(timestamp)
+//   new Date(Date String)
+//   new Date(Numeric Values)
 
-// dateNow.setDate(35);
-// console.log(dateNow);
+//   Format
+//   - "Oct 25 1982"
+//   - "10/25/1982"
+//   - "1982-10-25" => ISO International Standard
+//   - "1982 10"
+//   - "1982"
+//   - "82"
+//   - 1982, 9, 25, 2, 10, 0
+//   - 1982, 9, 25
+//   - "1982-10-25T06:10:00Z"
 
-// dateNow.setFullYear(2020, 13);
-// console.log(dateNow);
+//   Date.parse("String") // Read Date From A String
+// */
 
-dateNow.setMonth(15);
-console.log(dateNow);
+// console.log(Date.parse("Oct 25 1982"));
+
+// let date1 = new Date(0);
+// console.log(date1);
+
+// let date2 = new Date(404344800000);
+// console.log(date2);
+
+// let date3 = new Date("10-25-1982");
+// console.log(date3);
+
+// let date4 = new Date("1982-10-25");
+// console.log(date4);
+
+// let date5 = new Date("1982-10");
+// console.log(date5);
+
+// let date6 = new Date("82");
+// console.log(date6);
+
+// let date7 = new Date(1982, 9, 25, 2, 10, 0);
+// console.log(date7);
+
+// let date8 = new Date(1982, 9, 25);
+// console.log(date8);
+
+// let date9 = new Date("1982-10-25T06:10:00Z");
+// console.log(date9);
+//=================
+//=================
+// /*
+//   Date And Time
+//   - Track Operations Time
+
+//   Search
+//   - performance.now()
+//   - performance.mark()
+// */
+
+// // Start Time
+// let start = new Date();
+
+// // Operation
+// for (let i = 0; i < 100000; i++) {
+//   // document.write(`<div>${i}</div>`);
+//   let div = document.createElement("div");
+//   div.appendChild(document.createTextNode(i));
+//   document.body.appendChild(div);
+// }
+
+// // Time End
+// let end = new Date();
+
+// // Operation Duration
+// let duration = end - start;
+
+// console.log(duration);
+
 //=================
 //=================
 
