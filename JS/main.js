5e315e2e27241f5aac4fb6ae088235649c0bd9f9
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-24 09:40:42 +0300

    Promise, Then, Catch & Finally.

--- JS/main.js
+++ JS/main.js
@@ -3669,48 +3669,155 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 // ==========================
 // ==========================
 
+// /*
+//   To Understand Ajax, Fetch, Promises
+
+//   Pyramid Of Doom || Callback Hell
+
+//   - What Is Callback
+//   - Callback Hell Example
+
+//   What Is Callback
+//   - A Function That Is Passed Into Another One As An Argument To Be Executed Later
+//   - Function To Handle Photos
+//   --- [1] Download Photo From URL
+//   --- [2] Resize Photo
+//   --- [3] Add Logo To The Photo
+//   --- [4] Show The Photo In Website
+// */
+
+// function makeItRed(e) {
+//   e.target.style.color = "red";
+// }
+
+// let p = document.querySelector(".test");
+// p.addEventListener("click", makeItRed);
+
+// function iamACallback() {
+//   console.log("Iam A Callback Function");
+// }
+
+// setTimeout(iamACallback, 2000);
+
+// setTimeout(() => {
+//   console.log("Download Photo From URL");
+//   setTimeout(() => {
+//     console.log("Resize Photo");
+//     setTimeout(() => {
+//       console.log("Add Logo To The Photo");
+//       setTimeout(() => {
+//         console.log("Show The Photo In Website");
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+// ==========================
+// ==========================
 /*
-  To Understand Ajax, Fetch, Promises
+  Promise Intro And Syntax
+  - Promise In JavaScript Is Like Promise In Real Life
+  - Promise Is Something That Will Happen In The Future
+  - Promise Avoid Callback Hell
+  - Promise Is The Object That Represent The Status Of An Asynchronous Operation And Its Resulting Value
 
-  Pyramid Of Doom || Callback Hell
+  - Promise Status
+  --- Pending: Initial State
+  --- Fulfilled: Completed Successfully
+  --- Rejected: Failed
 
-  - What Is Callback
-  - Callback Hell Example
+  Story
+  - Once A Promise Has Been Called, It Will Start In A Pending State
+  - The Created Promise Will Eventually End In A Resolved State Or In A Rejected State
+  - Calling The Callback Functions (Passed To Then And Catch) Upon Finishing.
 
-  What Is Callback
-  - A Function That Is Passed Into Another One As An Argument To Be Executed Later
-  - Function To Handle Photos
-  --- [1] Download Photo From URL
-  --- [2] Resize Photo
-  --- [3] Add Logo To The Photo
-  --- [4] Show The Photo In Website
+  - Then
+  --- Takes 2 Optional Arguments [Callback For Success Or Failure]
 */
 
-function makeItRed(e) {
-  e.target.style.color = "red";
-}
+// const myPromise = new Promise((resolveFunction, rejectFunction) => {
+//   let connect = false;
+//   if (connect) {
+//     resolveFunction("Connection Established");
+//   } else {
+//     rejectFunction(Error("Connection Failed"));
+//   }
+// }).then(
+//   (resolveValue) => console.log(`Good ${resolveValue}`),
+//   (rejectValue) => console.log(`Bad ${rejectValue}`)
+// );
 
-let p = document.querySelector(".test");
-p.addEventListener("click", makeItRed);
+// const myPromise = new Promise((resolveFunction, rejectFunction) => {
+//   let connect = true;
+//   if (connect) {
+//     resolveFunction("Connection Established");
+//   } else {
+//     rejectFunction(Error("Connection Failed"));
+//   }
+// });
 
-function iamACallback() {
-  console.log("Iam A Callback Function");
-}
+// console.log(myPromise);
 
-setTimeout(iamACallback, 2000);
+// let resolver = (resolveValue) => console.log(`Good ${resolveValue}`);
+// let rejecter = (rejectValue) => console.log(`Bad ${rejectValue}`);
 
-setTimeout(() => {
-  console.log("Download Photo From URL");
-  setTimeout(() => {
-    console.log("Resize Photo");
-    setTimeout(() => {
-      console.log("Add Logo To The Photo");
-      setTimeout(() => {
-        console.log("Show The Photo In Website");
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
+// myPromise.then(resolver, rejecter);
+
+// myPromise.then(
+//   (resolveValue) => console.log(`Good ${resolveValue}`),
+//   (rejectValue) => console.log(`Bad ${rejectValue}`)
+// );
+
+// myPromise.then(
+//   (resolveValue) => console.log(`Good ${resolveValue}`),
+//   (rejectValue) => console.log(`Bad ${rejectValue}`)
+// );
+
+// ==========================
+// ==========================
+/*
+  Promise Training
+
+  We Will Go To The Meeting, Promise Me That We Will Find The 4 Employees
+  .then(We Will Choose Two People)
+  .then(We Will Test Them Then Get One Of Them)
+  .catch(No One Came)
+
+  Then    => Promise Is Successfull Use The Resolved Data
+  Catch   => Promise Is Failed, Catch The Error
+  Finally => Promise Successfull Or Failed Finally Do Something
+*/
+
+const myPromise = new Promise((resolveFunction, rejectFunction) => {
+  let employees = [];
+  if (employees.length === 4) {
+    resolveFunction(employees);
+  } else {
+    rejectFunction(Error("Number Of Employees Is Not 4"));
+  }
+});
+
+myPromise
+  .then((resolveValue) => {
+    resolveValue.length = 2;
+    return resolveValue;
+  })
+  .then((resolveValue) => {
+    resolveValue.length = 1;
+    return resolveValue;
+  })
+  .then((resolveValue) => {
+    console.log(`The Choosen Emplyee Is ${resolveValue}`);
+  })
+  .catch((rejectedReason) => console.log(rejectedReason))
+  .finally(console.log("The Operation Is Done"));
+
+
+
+// ==========================
+// ==========================
+
+
+
 // ==========================
 // ==========================
 
@@ -3760,7 +3867,7 @@ setTimeout(() => {
 
   - [ ]  XMLHttpRequest
   
-  - [ ]
+  - [ ]  Promise, Then.
 */
 
 
