47bdbeab642bac4ec18c7039e06fa219982e4d05
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-24 04:25:49 +0300

    JSON

--- JS/main.js
+++ JS/main.js
@@ -3342,11 +3342,123 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 
 //=================
 //=================
+// /*
+//   Modules
+//   - Import And Export
+// */
+
+// The module file
+// let a = 10;
+// let arr = [1, 2, 3, 4];
+
+// function saySomething() {
+//   return `Something`;
+// }
+
+// export { a, arr, saySomething };
+//===========================
+//The second file that will import 
+
+// import { a, arr, saySomething as s } from "./main.js";
+
+// console.log(a);
+// console.log(arr);
+// console.log(s());
+//=================
+//=================
+
+/*
+  What Is JSON ?
+  - JavaScript Object Notation
+  - Format For Sharing Data Between Server And Client
+  - JSON Derived From JavaScript
+  - Alternative To XML
+  - File Extension Is .json
+
+  Why JSON ?
+  - Easy To Use And Read
+  - Used By Most Programming Languages And Its Frameworks
+  - You Can Convert JSON Object To JS Object And Vice Versa
+
+  JSON vs XML
+  ===================================================
+  = Text Based Format      = Markup Language        =
+  = Lightweight            = Heavier                =
+  = Does Not Use Tags      = Using Tags             =
+  = Shorter                = Not Short              =
+  = Can Use Arrays         = Cannot Use Arrays      =
+  = Not Support Comments   = Support Comments       =
+  ===================================================
+*/
+// //JSON VS XML
+// {
+//   "widget": {
+//     "debug": "on",
+//     "window": {
+//       "title": "Sample Konfabulator Widget",
+//       "name": "main_window",
+//       "width": 500,
+//       "height": 500
+//     },
+//     "image": {
+//       "src": "Images/Sun.png",
+//       "name": "sun1",
+//       "hOffset": 250,
+//       "vOffset": 250,
+//       "alignment": "center"
+//     },
+//     "text": {
+//       "data": "Click Here",
+//       "size": 36,
+//       "style": "bold",
+//       "name": "text1",
+//       "hOffset": 250,
+//       "vOffset": 100,
+//       "alignment": "center",
+//       "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+//     }
+//   }
+// }
 
 
+// XML
 
+/*
+<widget>
+  <debug>on</debug>
+  <window title="Sample Konfabulator Widget">
+    <name>main_window</name>
+    <width>500</width>
+    <height>500</height>
+  </window>
+  <image src="Images/Sun.png" name="sun1">
+    <hOffset>250</hOffset>
+    <vOffset>250</vOffset>
+    <alignment>center</alignment>
+  </image>
+  <text data="Click Here" size="36" style="bold">
+    <name>text1</name>
+    <hOffset>250</hOffset>
+    <vOffset>100</vOffset>
+    <alignment>center</alignment>
+    <onMouseUp>
+      sun1.opacity = (sun1.opacity / 100) * 90;
+    </onMouseUp>
+  </text>
+</widget>
+*/
+// //JSON FILE
+// {
+//   "string": "Elzero",
+//   "number": 100,
+//   "object": { "EG": "Giza", "KSA": "Riyadh" },
+//   "array": ["HTML", "CSS", "JS"],
+//   "boolean": true,
+//   "null": null
+// }
 
-
+// //Website
+// //https://jsoneditoronline.org/#left=local.viqupi&right=local.kufimi
 
 
 
