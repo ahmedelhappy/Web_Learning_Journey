31018aee6a197f83f8ba9676f9a283c12d175fc2
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-26 07:44:31 +0300

    Promise And XHR

--- JS/main.js
+++ JS/main.js
@@ -3774,49 +3774,74 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 
 // ==========================
 // ==========================
+// /*
+//   Promise Training
+
+//   We Will Go To The Meeting, Promise Me That We Will Find The 4 Employees
+//   .then(We Will Choose Two People)
+//   .then(We Will Test Them Then Get One Of Them)
+//   .catch(No One Came)
+
+//   Then    => Promise Is Successfull Use The Resolved Data
+//   Catch   => Promise Is Failed, Catch The Error
+//   Finally => Promise Successfull Or Failed Finally Do Something
+// */
+
+// const myPromise = new Promise((resolveFunction, rejectFunction) => {
+//   let employees = [];
+//   if (employees.length === 4) {
+//     resolveFunction(employees);
+//   } else {
+//     rejectFunction(Error("Number Of Employees Is Not 4"));
+//   }
+// });
+
+// myPromise
+//   .then((resolveValue) => {
+//     resolveValue.length = 2;
+//     return resolveValue;
+//   })
+//   .then((resolveValue) => {
+//     resolveValue.length = 1;
+//     return resolveValue;
+//   })
+//   .then((resolveValue) => {
+//     console.log(`The Choosen Emplyee Is ${resolveValue}`);
+//   })
+//   .catch((rejectedReason) => console.log(rejectedReason))
+//   .finally(console.log("The Operation Is Done"));
+
+
+
+// ==========================
+// ==========================
 /*
-  Promise Training
-
-  We Will Go To The Meeting, Promise Me That We Will Find The 4 Employees
-  .then(We Will Choose Two People)
-  .then(We Will Test Them Then Get One Of Them)
-  .catch(No One Came)
-
-  Then    => Promise Is Successfull Use The Resolved Data
-  Catch   => Promise Is Failed, Catch The Error
-  Finally => Promise Successfull Or Failed Finally Do Something
+  Promise And XHR
 */
 
-const myPromise = new Promise((resolveFunction, rejectFunction) => {
-  let employees = [];
-  if (employees.length === 4) {
-    resolveFunction(employees);
-  } else {
-    rejectFunction(Error("Number Of Employees Is Not 4"));
-  }
-});
+const getData = (apiLink) => {
+  return new Promise((resolve, reject) => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.onload = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        resolve(JSON.parse(this.responseText));
+      } else {
+        reject(Error("No Data Found"));
+      }
+    };
 
-myPromise
-  .then((resolveValue) => {
-    resolveValue.length = 2;
-    return resolveValue;
+    myRequest.open("GET", apiLink);
+    myRequest.send();
+  });
+};
+
+getData("https://api.github.com/users/elzerowebschool/repos")
+  .then((result) => {
+    result.length = 10;
+    return result;
   })
-  .then((resolveValue) => {
-    resolveValue.length = 1;
-    return resolveValue;
-  })
-  .then((resolveValue) => {
-    console.log(`The Choosen Emplyee Is ${resolveValue}`);
-  })
-  .catch((rejectedReason) => console.log(rejectedReason))
-  .finally(console.log("The Operation Is Done"));
-
-
-
-// ==========================
-// ==========================
-
-
+  .then((result) => console.log(result[0].name))
+  .catch((rej) => console.log(rej));
 
 // ==========================
 // ==========================
