8ba280b9fdcb79000a7090ec12a975224df39932
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-07-26 09:14:03 +0300

    Async

--- JS/main.js
+++ JS/main.js
@@ -3941,6 +3941,61 @@ let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
 // */
 // ==========================
 // ==========================
+// /*
+//   Async
+//   - Async Before Function Mean This Function Return A Promise
+//   - Async And Await Help In Creating Asynchronous Promise Behavior With Cleaner Style
+// */
+
+// // function getData() {
+// //   return new Promise((res, rej) => {
+// //     let users = [];
+// //     if (users.length > 0) {
+// //       res("Users Found");
+// //     } else {
+// //       rej("No Users Found");
+// //     }
+// //   });
+// // }
+
+// // getData().then(
+// //   (resolvedValue) => console.log(resolvedValue),
+// //   (rejectedValue) => console.log("Rejected " + rejectedValue)
+// // );
+
+// // function getData() {
+// //   let users = ["Osama"];
+// //   if (users.length > 0) {
+// //     return Promise.resolve("Users Found");
+// //   } else {
+// //     return Promise.reject("No Users Found");
+// //   }
+// // }
+
+// // getData().then(
+// //   (resolvedValue) => console.log(resolvedValue),
+// //   (rejectedValue) => console.log("Rejected " + rejectedValue)
+// // );
+
+// async function getData() {
+//   let users = ["Ahmed"];
+//   if (users.length > 0) {
+//     return "Users Found";
+//   } else {
+//     return "No Users Found";
+//   }
+// }
+
+// console.log(getData());
+
+// getData().then(
+//   (resolvedValue) => console.log(resolvedValue),
+//   (rejectedValue) => console.log("Rejected " + rejectedValue)
+// );
+
+
+// ==========================
+// ==========================
 
 
 
