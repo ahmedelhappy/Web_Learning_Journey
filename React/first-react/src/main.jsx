d7fbd6939682d191eab69c832a6e106b2c45e12f
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-08-12 01:44:44 +0300

    Declarative React

--- React/first-react/src/main.jsx
+++ React/first-react/src/main.jsx
@@ -1,6 +1,6 @@
-// import { createElement } from "react"
-import {createRoot} from "react-dom/client"
-const root = createRoot(document.querySelector("#root"))
+import { createElement } from "react"
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.querySelector("#root"));
 
 // root.render(<h1>Hello</h1>)
 // const createEle = createElement("h1", null, "Hi From createElement!")
@@ -9,8 +9,33 @@ const root = createRoot(document.querySelector("#root"))
 // //createElement wasn't the best when it comes to nested elements
 
 //JSX Syntax
-const createEle = <h1>Hi From <span>Nested Span</span> JSX!</h1>
-root.render(createEle);
+// const createEle = (
+//   <h1>
+//     Hi From <span>Nested Span</span> JSX!
+//   </h1>
+// );
+// root.render(createEle);
 
+//===========================================
+// function MainContent() {
+//   return <h1>React is great!</h1>;
+// }
 
+// root.render(
+//   <div>
+//     <MainContent />
+//   </div>
+// );
+
+// //=======================Declarative React============================
+// // Using js, it's Imperative (have to tell every single step)
+// let myElement = document.createElement("h1");
+// myElement.textContent = "Hello, React!";
+// myElement.classList.add("header");
+// document.body.append(myElement);
+
+// // React is Declarative, (just ask what do you want to do)
+// root.render(
+//     <h1 className="header">Hello, React!</h1>
+// )
 
