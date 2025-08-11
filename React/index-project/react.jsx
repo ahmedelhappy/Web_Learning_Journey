e206d58e71ead77cbe07f4bc51a35a70f46f88fa
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-08-11 04:15:12 +0300

    Add the first react Project, using Vite.

---
+++ React/index-project/react.jsx
@@ -0,0 +1,7 @@
+import {createRoot} from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"))
+root.render(<h1>Hello, React!</h1>)
+
+//found out it's not gonna work.. without vite
+
