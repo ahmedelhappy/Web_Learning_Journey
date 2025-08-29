8bf2440703747c4657a0596366a5faa99fe3218f
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-08-30 02:55:45 +0300

    small edit in the interval example.

--- js2/script.js
+++ js2/script.js
@@ -16,20 +16,24 @@ console.log(`Min: ${min}`);
 console.log(`Max: ${max}`);
 */
 
-
 let button1 = document.querySelector(".btn");
 let button2 = document.querySelector(".btn2");
-let counter = 0;
+let h1 = document.querySelector(".h1");
+let counter = h1.textContent;
 let intervalId;
+let running = false;
 
 function startIntervalFunc() {
-  intervalId = setInterval(function () {
-    button1.innerHTML = counter;
-    counter++;
-  }, 1000);
+  if (!running) {
+    intervalId = setInterval(function () {
+      ++counter;
+      h1.innerHTML = counter;
+    }, 1000);
+    running = true;
+  }
 }
 
 function stopIntervalFunc() {
   clearInterval(intervalId);
+  running = false;
 }
-
