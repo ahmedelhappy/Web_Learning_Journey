2f614275bccf66d54a4e819a80c713c8f2208fc7
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-10-17 11:58:21 +0300

    exercise 3c

--- React2/3-lesson3/3-exercises.js
+++ React2/3-lesson3/3-exercises.js
@@ -1,12 +1,11 @@
 let root = ReactDOM.createRoot(document.querySelector(".js-container"));
 
-
 /* 3a Exercise */
 // function CounterButton() {
 //   function displayClicked() {
 //     console.log("clicked")
 //   }
-  
+
 //   return(
 //   <>
 //     <button onClick={displayClicked} >Clicked 0 times</button>
@@ -24,38 +23,62 @@ let root = ReactDOM.createRoot(document.querySelector(".js-container"));
 // root.render(<App />)
 /* ------------------------- */
 
-
 /* 3b Exercise */
-function CounterButton() {  
-  // const array = React.useState(0);
-  // let count = array[0];
-  // const setCount = array[1];
+// function CounterButton() {
+//   // const array = React.useState(0);
+//   // let count = array[0];
+//   // const setCount = array[1];
 
+//   let [count, setCount] = React.useState(0);
+
+//   function increaseCount() {
+//     setCount(++count);
+//     console.log("clicked")
+//   }
+
+//   return(
+//   <>
+//     <button onClick={increaseCount} >Clicked {count} times</button>
+//   </>)
+// }
+
+// function App() {
+//   return(
+//     <>
+//       <CounterButton />
+//     </>
+//   )
+// }
+
+// root.render(<App />)
+/* ------------------------- */
+
+/* 3c Exercise */
+function CounterButton() {
   let [count, setCount] = React.useState(0);
 
   function increaseCount() {
     setCount(++count);
-    console.log("clicked")
   }
-  
-  return(
-  <>
-    <button onClick={increaseCount} >Clicked {count} times</button>
-  </>)
+
+  return (
+    <>
+      <button onClick={increaseCount}>
+        Clicked {count} {count === 1 ? "time" : "times"}
+      </button>
+    </>
+  );
 }
 
 function App() {
-  return(
+  return (
     <>
       <CounterButton />
     </>
-  )
+  );
 }
 
-root.render(<App />)
-/* ------------------------- */
-
-/* 3c Exercise */
+root.render(<App />);
 /* ------------------------- */
 
 /* 3d Exercise */
