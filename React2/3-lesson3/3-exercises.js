ae8b61fe3e0140f201d0660aacc9a45d0628e263
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-10-17 11:44:46 +0300

    exercise 3a

---
+++ React2/3-lesson3/3-exercises.js
@@ -0,0 +1,62 @@
+let root = ReactDOM.createRoot(document.querySelector(".js-container"));
+
+
+/* 3a Exercise */
+
+function CounterButton() {
+  function displayClicked() {
+    console.log("clicked")
+  }
+  
+  return(
+  <>
+    <button onClick={displayClicked} >Clicked 0 times</button>
+  </>)
+}
+
+function App() {
+  return(
+    <>
+      <CounterButton />
+    </>
+  )
+}
+
+root.render(<App />)
+/* ------------------------- */
+
+/* 3b Exercise */
+/* ------------------------- */
+
+/* 3c Exercise */
+/* ------------------------- */
+
+/* 3d Exercise */
+/* ------------------------- */
+
+/* 3e Exercise */
+/* ------------------------- */
+
+/* 3f Exercise */
+/* ------------------------- */
+
+/* 3g Exercise */
+/* ------------------------- */
+
+/* 3h Exercise */
+/* ------------------------- */
+
+/* 3i Exercise */
+/* ------------------------- */
+
+/* 3j Exercise */
+/* ------------------------- */
+
+/* 3k Exercise */
+/* ------------------------- */
+
+/* 3l Exercise */
+/* ------------------------- */
+
+/* 3m Exercise */
+/* ------------------------- */
