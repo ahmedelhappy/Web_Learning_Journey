16eac7662b14c22f2a1d55c82aa3d63a3434b6bb
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-10-23 17:45:26 +0300

    4e exercise

--- React2/4-lesson4/exercises/4-exercises.js
+++ React2/4-lesson4/exercises/4-exercises.js
@@ -1,4 +1,3 @@
-
 /* 4a Exercise */
 // const container = document.querySelector(".js-container");
 // ReactDOM.createRoot(container).render(<App />);
@@ -11,7 +10,6 @@
 
 /* ------------------------- */
 
-
 /* 4b Exercise */
 // const container = document.querySelector(".js-container");
 // ReactDOM.createRoot(container).render(<App />);
@@ -20,7 +18,7 @@
 //   const [isButtonOn, setIsButtonOn] = React.useState(true);
 
 //   return (
-//     <button 
+//     <button
 //     className="is-button-on"
 //     onClick={() => isButtonOn? setIsButtonOn(false) : setIsButtonOn(true)}
 //     >
@@ -30,8 +28,7 @@
 // }
 /* ------------------------- */
 
-
- /* 4c Exercise */
+/* 4c Exercise */
 // const container = document.querySelector(".js-container");
 // ReactDOM.createRoot(container).render(<App />);
 
@@ -39,7 +36,7 @@
 //   const [isButtonOn, setIsButtonOn] = React.useState(true);
 
 //   return (
-//     <button 
+//     <button
 //     className = {isButtonOn ? "is-button-on" : "is-button-off"}
 //     onClick={() => isButtonOn? setIsButtonOn(false) : setIsButtonOn(true)}
 //     >
@@ -50,56 +47,73 @@
 
 /* ------------------------- */
 
+/* 4d Exercise */
+// const container = document.querySelector(".js-container");
+// ReactDOM.createRoot(container).render(<App />);
 
- /* 4d Exercise */
+// function App() {
+
+//   return (
+//     <>
+//       <p>Hello, welcome to my website</p>
+//       <input placeholder="Email" type="email" />
+//       <br />
+//       <input placeholder="Password" type="Password" />
+//       <br />
+//       <button>Login</button>
+//       <button>Sign Up</button>
+//     </>
+//   )
+// }
+
+/* ------------------------- */
+
+/* 4e Exercise */
 const container = document.querySelector(".js-container");
 ReactDOM.createRoot(container).render(<App />);
 
 function App() {
+  const [showPass, setShowPass] = React.useState(false);
 
   return (
     <>
       <p>Hello, welcome to my website</p>
       <input placeholder="Email" type="email" />
       <br />
-      <input placeholder="Password" type="Password" />
+      <div className="pass-container">
+        <input placeholder="Password" type={!showPass ? "password" : "text"} />
+        <button
+          className="show-password-button"
+          onClick={() => (showPass ? setShowPass(false) : setShowPass(true))}
+        >
+          <img src="https://cdn-icons-png.flaticon.com/128/2767/2767146.png"></img>
+        </button>
+      </div>
       <br />
-      <button>Login</button>
-      <button>Sign Up</button>
+      <button className="login-button">Login</button>
+      <button className="sign-up-button">Sing up</button>
     </>
-  )
+  );
 }
-
 /* ------------------------- */
 
-
- /* 4e Exercise */
+/* 4f Exercise */
 /* ------------------------- */
 
-
- /* 4f Exercise */
+/* 4g Exercise */
 /* ------------------------- */
 
-
- /* 4g Exercise */
+/* 4h Exercise */
 /* ------------------------- */
 
-
- /* 4h Exercise */
+/* 4i Exercise */
 /* ------------------------- */
 
-
- /* 4i Exercise */
+/* 4j Exercise */
 /* ------------------------- */
 
-
- /* 4j Exercise */
+/* 4k Exercise */
 /* ------------------------- */
 
-
- /* 4k Exercise */
-/* ------------------------- */
-
-
- /* 4l Exercise */
+/* 4l Exercise */
 /* ------------------------- */
