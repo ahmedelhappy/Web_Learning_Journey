0a427dd4036c8819712b7816cf668ea11b57431d
Ahmed Elsaeed <ahmedelhappy@gmail.com>
2025-10-23 08:20:25 +0300

    exercise 4a

---
+++ React2/4-lesson4/exercises/4-exercises.js
@@ -0,0 +1,56 @@
+const container = document.querySelector(".js-container");
+ReactDOM.createRoot(container).render(<App />);
+
+function App() {
+  return (
+    <button className="no-button">No</button>
+  )
+}
+
+/* 4a Exercise */
+
+/* ------------------------- */
+
+
+/* 4b Exercise */
+/* ------------------------- */
+
+
+ /* 4c Exercise */
+/* ------------------------- */
+
+
+ /* 4d Exercise */
+/* ------------------------- */
+
+
+ /* 4e Exercise */
+/* ------------------------- */
+
+
+ /* 4f Exercise */
+/* ------------------------- */
+
+
+ /* 4g Exercise */
+/* ------------------------- */
+
+
+ /* 4h Exercise */
+/* ------------------------- */
+
+
+ /* 4i Exercise */
+/* ------------------------- */
+
+
+ /* 4j Exercise */
+/* ------------------------- */
+
+
+ /* 4k Exercise */
+/* ------------------------- */
+
+
+ /* 4l Exercise */
+/* ------------------------- */
